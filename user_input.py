0a02bb2d229dcf77936169a6ff9c48c5c5f9ff39
Raphael Thomas <42302012+raphthtech@users.noreply.github.com>
2024-11-03 18:13:20 +0100

    Create user_input.py

---
+++ user_input.py
@@ -0,0 +1,6 @@
+firstName = input('Enter your name: ')
+lastName = input('Enter your last name ')
+age = input('Enter your age ')
+location = input('What is your location? ')
+
+print(f'Welcome {firstName} {lastName}. Please confirm that you are {age}years old and live in {location}')
